0b3e506b457c9a6b321d9d5574ea0f81928270a8
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-03-30 19:12:00 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,44 @@ public class PracticeProblem {
 
 	}
 
-	
-
+	public static String [] selectionSortName(String [] names, int [] ages){
+		int tempNum;
+		String tempName;
+		int min;
+		for (int i = 0;i < names.length - 1;i ++){
+			min = i;
+			for (int j = i + 1;j < names.length;j++){
+				if ((names[j].compareToIgnoreCase(names[min]) < 0)){
+					min = j;
+				}
+			}
+			tempName = names[min];
+			names[min] = names[i];
+			names[i] = tempName;
+			tempNum = ages[min];
+			ages[min] = ages[i];
+			ages[i] = tempNum;
+		}
+		return names;
+	}
+	public static int [] selectionSortAge(String [] names, int [] ages){
+		int tempNum;
+		String tempName;
+		int min;
+		for (int i = 0;i < names.length - 1;i++){
+			min = i;
+			for(int j = i + 1;j < names.length;j++){
+				if (ages[j] < ages[min]){
+					min = j;
+				}
+			}
+			tempName = names[min];
+			names[min] = names[i];
+			names[i] = tempName;
+			tempNum = ages[min];
+			ages[min] = ages[i];
+			ages[i] = tempNum;
+		}
+		return ages;
+	}
 }
